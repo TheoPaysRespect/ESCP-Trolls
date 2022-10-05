5c8715d12a3f9c8a74fede60ac882aecd0904e98
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-10-05 18:36:33 +1300

    Updated to ESCP

---
+++ 1.3/Source/ESCP_Trolls/ESCP_Trolls/HediffComps/HediffComp_Regeneration.cs
@@ -0,0 +1,39 @@
+﻿using Verse;
+using RimWorld;
+
+namespace ESCP_Trolls
+{
+    public class HediffComp_Regeneration : HediffComp
+	{
+
+		public int ticks = 0;
+
+		public HediffCompProperties_Regeneration Props
+		{
+			get
+			{
+				return (HediffCompProperties_Regeneration)this.props;
+			}
+		}
+
+		public override void CompPostTick(ref float severityAdjustment)
+		{
+			base.CompPostTick(ref severityAdjustment);
+			Pawn pawn = base.Pawn;
+			if (ticks >= Props.Ticks)
+			{
+				if (pawn.IsBurning() || pawn.Dead || (Props.OrsimerRage && parent.Severity < 0.8f))
+				{
+					ticks = 0;
+					return;
+				}
+				else
+				{
+					Utility.HealWounds(pawn, Props.BaseNumber, Props.Severity, Props.Tend);
+				}
+				ticks = 0;
+			}
+			else ticks++;
+		}
+	}
+}
---
+++ 1.3/Source/ESCP_Trolls/ESCP_Trolls/Utility.cs
@@ -0,0 +1,52 @@
+﻿using Verse;
+using RimWorld;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ESCP_Trolls
+{
+    public static class Utility
+    {
+        public static void HealWounds(Pawn pawn, float baseNumber, float severityValue, float tendValue)
+        {
+			float CurrentNumber = baseNumber * pawn.health.capacities.GetLevel(PawnCapacityDefOf.BloodPumping);
+			for (int i = 0; i != baseNumber; i++)
+			{
+				Hediff hediff;
+				if (!(from hd in pawn.health.hediffSet.hediffs
+					  where hd.Bleeding
+					  select hd).TryRandomElement(out hediff))
+				{
+					return;
+				}
+				if (pawn.health.hediffSet.PartIsMissing(hediff.Part))
+				{
+					hediff.Tended(tendValue, 1);
+				}
+				else
+				{
+					hediff.Severity -= severityValue;
+				}
+			}
+		}
+    }
+
+	[StaticConstructorOnStartup]
+	public static class Utility_OnStartup
+	{
+		public static List<PawnKindDef> trollKindDefs = new List<PawnKindDef> { };
+		public static void FetchTrollKindDefs()
+		{
+			List<PawnKindDef> allDefsListForReading = DefDatabase<PawnKindDef>.AllDefsListForReading;
+			for (int i = 0; i < allDefsListForReading.Count; i++)
+			{
+				PawnKindDef pawnKindDef = allDefsListForReading[i];
+				bool flag = pawnKindDef.race.race.Animal && pawnKindDef.race.tradeTags != null && pawnKindDef.race.tradeTags.Contains("ESCP_Troll");
+				if (flag)
+				{
+					trollKindDefs.Add(pawnKindDef);
+				}
+			}
+		}
+	}
+}
