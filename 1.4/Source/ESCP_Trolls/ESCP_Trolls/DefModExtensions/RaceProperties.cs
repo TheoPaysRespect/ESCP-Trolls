317e8256ceec46ed3694e0b9700a16a5c2a29aa5
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2023-05-03 09:44:59 +1200

    Update to 1.0.10

--- 1.4/Source/ESCP_Trolls/ESCP_Trolls/DefModExtensions/RaceProperties.cs
+++ 1.4/Source/ESCP_Trolls/ESCP_Trolls/DefModExtensions/RaceProperties.cs
@@ -9,12 +9,6 @@ namespace ESCP_Trolls
         public bool scaleExtraButcher = false;
         public List<ThingDef> extraButcherItems;    //specifically ones to be scaled
 
-        public float chanceToRequire = 1f;
-        public bool requireRiver = false;
-        public bool requireCoast = false;
-        public bool requireCaves = false;
-        public bool requireHills = false;
-
         public static RaceProperties Get(Def def)
         {
             return def.GetModExtension<RaceProperties>();
